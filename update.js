8754d6a33fb4401f994d0125786a993e567b5a8c
irynafill <85508855+irynafill@users.noreply.github.com>
2023-02-03 10:15:01 +0200

    Create update.js
    added file update.js

---
+++ update.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+var packagejson = require('./lens/packages/open-lens/package.json');
+
+packagejson.build.publish = [{
+    url: "https://github.com/irynafill/OpenLens/releases/download/Latest",
+    provider: "generic"
+}];
+
+packagejson.build.win.artifactName = "OpenLens.Setup.${version}.${ext}";
+
+if (process.platform != "win32") {
+    // build both x86_64 and arm64 for Linux and Darwin
+    packagejson.scripts['build:app'] = "electron-builder --publish onTag --x64 --arm64";
+}
+
+fs.writeFileSync('./lens/packages/open-lens/package.json', JSON.stringify(packagejson));
